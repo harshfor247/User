74cc60bdd7839d927a9e97dc27b9e2c19ea17c55
Harsh Shivhare <harshshivhare247@gmail.com>
2025-08-04 11:39:40 +0530

    Junit test Cases

--- src/test/java/com/example/User/service/UserServiceTest.java
+++ src/test/java/com/example/User/service/UserServiceTest.java
@@ -2,18 +2,20 @@ package com.example.User.service;
 
 import com.example.User.dto.request.UserBalanceRequest;
 import com.example.User.dto.request.UserRequest;
+import com.example.User.dto.request.UserUpdateRequest;
 import com.example.User.dto.response.UserResponse;
 import com.example.User.entity.User;
+import com.example.User.enums.UserStatus;
 import com.example.User.exceptions.EmailAlreadyExistException;
 import com.example.User.exceptions.MobileNumberAlreadyExistException;
 import com.example.User.exceptions.UserIdException;
 import com.example.User.kafka.producer.BalanceProducer;
 import com.example.User.repository.UserRepository;
-import com.example.User.util.CommonObjects;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
@@ -111,7 +113,6 @@ public class UserServiceTest {
         //when + then
         UserIdException exception = assertThrows(UserIdException.class,
                 () -> userService.getUser(userId));
-
         assertEquals("User does not exist with ID: 99", exception.getMessage());
 
         //verify
@@ -140,5 +141,85 @@ public class UserServiceTest {
         //verify
         verify(userRepository).findByUserId(userId);
         verify(objectMapper).convertValue(forUser, UserResponse.class);
+    }
+
+    @Test
+    void test_updateUser_UserNotExists(){
+
+        //given
+        Long userId = 987L;
+        UserUpdateRequest forUserUpdateRequest = sampleUserUpdateRequest();
+        when(userRepository.findByUserId(userId)).thenReturn(Optional.empty());
+
+        //when
+        UserIdException exception = assertThrows(UserIdException.class,
+                () -> userService.updateUser(forUserUpdateRequest));
+
+        //then
+        assertEquals("User not Found!", exception.getMessage());
+
+        //verify
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void test_updateUser(){
+
+        //given
+        Long userId = 1L;
+        User forUser = sampleUser();
+        UserResponse expectedResponse = sampleUserUpdateResponse();
+        UserUpdateRequest forUserUpdateRequest = sampleUserUpdateRequest();
+        when(userRepository.findByUserId(userId)).thenReturn(Optional.of(forUser));
+        when(objectMapper.convertValue(forUser, UserResponse.class)).thenReturn(expectedResponse);
+
+        //when
+        ResponseEntity<UserResponse> actualResponse = userService.updateUser(forUserUpdateRequest);
+
+        //then
+        assertNotNull(actualResponse);
+        assertEquals(expectedResponse, actualResponse.getBody());
+
+        //verify
+        verify(userRepository).findByUserId(userId);
+        verify(objectMapper).convertValue(forUser, UserResponse.class);
+    }
+
+    @Test
+    void test_deactivateUser_UserNotExist(){
+
+        //given
+        Long userId = 987L;
+        when(userRepository.findByUserId(userId)).thenReturn(Optional.empty());
+
+        //when
+        UserIdException exception = assertThrows(UserIdException.class,
+                () -> userService.deactivateUser(userId));
+
+        //then
+        assertEquals("User does not exist with ID: 987", exception.getMessage());
+
+        //verify
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void test_deactivateUser(){
+
+        //given
+        Long userId = 1L;
+        User forUser = sampleUser();
+        when(userRepository.findByUserId(userId)).thenReturn(Optional.of(forUser));
+
+        //when
+        ResponseEntity<String> expectedResponse = userService.deactivateUser(userId);
+
+        //then
+        assertEquals(HttpStatus.OK, expectedResponse.getStatusCode());
+        assertEquals("User deactivated successfully", expectedResponse.getBody());
+        assertEquals(UserStatus.INACTIVE, forUser.getUserStatus());
+
+        //verify
+        verify(userRepository).save(forUser);
     }
 }
--- src/test/java/com/example/User/util/CommonObjects.java
+++ src/test/java/com/example/User/util/CommonObjects.java
@@ -1,6 +1,7 @@
 package com.example.User.util;
 
 import com.example.User.dto.request.UserRequest;
+import com.example.User.dto.request.UserUpdateRequest;
 import com.example.User.dto.response.UserResponse;
 import com.example.User.entity.User;
 import com.example.User.enums.UserStatus;
@@ -17,5 +18,13 @@ public class CommonObjects {
 
     public static User sampleUser(){
         return new User(1L, UserStatus.ACTIVE, "Alan", "9826198904", "alan@gmail.com", 24, 10000D);
+    }
+
+    public static UserUpdateRequest sampleUserUpdateRequest(){
+        return new UserUpdateRequest(1L, "Alan", "alan@gmail.com","8826198904", 24, 10000D);
+    }
+
+    public static UserResponse sampleUserUpdateResponse(){
+        return new UserResponse(1L, UserStatus.ACTIVE, "Alan", "8826198904", "alan@gmail.com", 24, 10000D);
     }
 }
